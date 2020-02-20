3cdde8b08903c90b406105de973921bd28ebd5e6
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-02-20 11:47:04 +0100

    Counters list, edit and view

---
+++ counters_list.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Counters - Counters List
+ *
+ * @package Coordinator\Modules\Counters
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+ api_checkAuthorization("counters-usage","dashboard");
+ // include module template
+ require_once(MODULE_PATH."template.inc.php");
+ // definitions
+ $users_array=array();
+ // set application title
+ $app->setTitle(api_text("counters_list"));
+ // definitions
+ $counters_array=array();
+ // build filter
+ $filter=new strFilter();
+ $filter->addSearch(["name","description","identifier"]);
+ // build query object
+ $query=new cQuery("counters__counters",$filter->getQueryWhere());
+ $query->addQueryOrderField("name");
+ // build pagination object
+ $pagination=new strPagination($query->getRecordsCount());
+ // cycle all results
+ foreach($query->getRecords($pagination->getQueryLimits()) as $result_f){$counters_array[$result_f->id]=new cCountersCounter($result_f);}
+ // build table
+ $table=new strTable(api_text("counters_list-tr-unvalued"));
+ $table->addHeader($filter->link(api_icon("fa-filter",api_text("filters-modal-link"),"hidden-link")),"text-center",16);
+ $table->addHeader(api_text("cCountersCounter-property-name"),null,"100%");
+ $table->addHeader(api_text("cCountersCounter-property-identifier"),"nowrap text-right");
+ $table->addHeader("&nbsp;",null,16);
+ // cycle all counters
+ foreach($counters_array as $counter_fobj){
+  // build operation button
+  $ob=new strOperationsButton();
+  $ob->addElement(api_url(["scr"=>"counters_edit","idCounter"=>$counter_fobj->id,"return"=>["scr"=>"counters_list"]]),"fa-pencil",api_text("table-td-edit"),(api_checkAuthorization("counters-manage")));
+  if($counter_fobj->deleted){$ob->addElement(api_url(["scr"=>"controller","act"=>"undelete","obj"=>"cCountersCounter","idCounter"=>$counter_fobj->id,"return"=>["scr"=>"counters_list"]]),"fa-trash-o",api_text("table-td-undelete"),(api_checkAuthorization("counters-manage")),api_text("cCountersCounter-confirm-undelete"));}
+  else{$ob->addElement(api_url(["scr"=>"controller","act"=>"delete","obj"=>"cCountersCounter","idCounter"=>$counter_fobj->id,"return"=>["scr"=>"counters_list"]]),"fa-trash",api_text("table-td-delete"),(api_checkAuthorization("counters-manage")),api_text("cCountersCounter-confirm-delete"));}
+  // make table row class
+  $tr_class_array=array();
+  if($counter_fobj->id==$_REQUEST['idCounter']){$tr_class_array[]="currentrow";}
+  if($counter_fobj->deleted){$tr_class_array[]="deleted";}
+  // make counters row
+  $table->addRow(implode(" ",$tr_class_array));
+  $table->addRowFieldAction(api_url(["scr"=>"counters_view","idCounter"=>$counter_fobj->id]),"fa-search",api_text("table-td-view"));
+  $table->addRowField($counter_fobj->name,"truncate-ellipsis");
+  $table->addRowField(api_tag("samp",$counter_fobj->identifier),"nowrap text-right");
+  $table->addRowField($ob->render(),"nowrap text-right");
+ }
+ // build grid object
+ $grid=new strGrid();
+ $grid->addRow();
+ $grid->addCol($filter->render(),"col-xs-12");
+ $grid->addRow();
+ $grid->addCol($table->render(),"col-xs-12");
+ $grid->addRow();
+ $grid->addCol($pagination->render(),"col-xs-12");
+ // add content to counter
+ $app->addContent($grid->render());
+ // renderize counter
+ $app->render();
+ // debug
+ api_dump($query,"query");
+?>
